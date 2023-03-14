1a1e72334035654331de45641a581fd8148a79c9
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-14 20:57:07 -0300

    criado

---
+++ SQL/QuerySelect.sql
@@ -0,0 +1,13 @@
+---selecionar a tabela---
+SELECT *
+FROM person.Person;
+--selecionar title--
+
+SELECT TITLE
+FROM person.Person;
+
+--selecionar tabela de emails---
+SELECT*
+FROM person.EmailAddress;
+
+
