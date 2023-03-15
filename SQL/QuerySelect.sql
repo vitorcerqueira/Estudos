fbc9ac2ecb9deba144c82e165148e331d3374936
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-14 21:43:47 -0300

    alterado

--- SQL/QuerySelect.sql
+++ SQL/QuerySelect.sql
@@ -3,11 +3,16 @@ SELECT *
 FROM person.Person;
 
 ---selecionar title---
-SELECT TITLE
+SELECT Title
 FROM person.Person;
 
 ---selecionar tabela de emails---
 SELECT*
 FROM person.EmailAddress;
+---selecionar nomes e sobrenomes---
+SELECT firstName,lastName
+FROM person.person;
+
+
 
 
