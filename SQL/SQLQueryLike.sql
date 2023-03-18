faedf0709c52e6be6a3e4f76d1630a736b58bf7f
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 18:53:44 -0300

    alterado

--- SQL/SQLQueryLike.sql
+++ SQL/SQLQueryLike.sql
@@ -1,1 +1,12 @@
-/*LIKE*/
+/*LIKE
+vamos dizer que voce quer encontrar uma pessoa no banco de dados 
+que voce sabe que o nome dela era ovi...alguma coisa
+*/
+SELECT* 
+FROM person.person
+WHERE Firstname like 'ovi%' /*o like ele vai ajudar a trazer alguma informaçao que eu nao souber todo o contexto
+% no fim ele vai mostrar tudo que vem antes da palavra*/
+
+SELECT* 
+FROM person.person
+WHERE Firstname like '%to'/*% se for no inicio ele vai mostrar o que vem no fim da palavra*/
