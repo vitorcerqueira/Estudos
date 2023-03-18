ec69c3d5253827ea937a6fede69af9ac854caa78
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 18:26:44 -0300

    alterado

--- SQL/SQLQueryBetween.sql
+++ SQL/SQLQueryBetween.sql
@@ -1,5 +1,23 @@
 --BETWEEN--
-/* o between é usado para encontrar valor entre um valor minimo e valor maximo 
-no caso o significado de between é "entre"*/
+/* o BETWEEN é usado para encontrar valor entre um valor minimo e valor maximo.
+
+o significado de BETWEEN é "entre".
+
+valor BETWEEN minimo and maximo.
+
+é a mesma coisa que dizer:
+valor >= minimo and valor <= maximo;
+*/
+---------------------------------------------------------
+/* selecionando a coluna listprice entre 1000 e 1500*/
+SELECT*
+FROM Production.Product
+WHERE ListPrice between 1000 and 1500;
+------------------------------------------
+/* selecionando a coluna listprice que nao esteje entre 1000 e 1500*/
+SELECT*
+FROM Production.Product
+WHERE ListPrice not between 1000 and 1500;
+------------------------------------------
 
 
