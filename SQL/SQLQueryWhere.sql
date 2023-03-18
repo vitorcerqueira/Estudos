f4345d9bc0d3acef7c73361f5c8bd28f1c3be88d
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 16:30:03 -0300

    alterado

--- SQL/SQLQueryWhere.sql
+++ SQL/SQLQueryWhere.sql
@@ -32,5 +32,39 @@ SELECT*
 FROM Production.Product
 WHERE ListPrice >= '1500' and ListPrice <= '4000'
 
+/*
+a equipe de produçao de produtos precisa do nome de todas as peças que pesam mais que 500kg mas nao mais que 700kg para
+para inspeção (peso em ingles weight)
+*/
+
+SELECT name
+FROM Production.Product
+WHERE weight >= '500' and Weight <= '700' 
+
+/*
+foi pedido pelo marketing uma relaçao de todos os empregados(ingles employees) que sao casados(ingles single=solteiro
+married=casado) e sao assalariados(ingles salaried)
+*/
+SELECT*
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'M' and SalariedFlag = '1'
+
+SELECT*
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'S' and SalariedFlag = '1'
+
+
+ /*
+ um usuario chamado Peter kredbs esta devendo um pagamento, consiga email dele para que possamos enviar uma cobraça 
+ */
+SELECT*
+FROM Person.Person
+WHERE FirstName ='peter' and LastName = 'krebs'
+
+SELECT*
+FROM person.EmailAddress
+WHERE BusinessEntityID = '26'  
+
+
 
 
