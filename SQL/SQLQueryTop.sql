b4431a65aaad6a836eb51df062ceb45ef704edd7
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 17:16:05 -0300

    alterado

--- SQL/SQLQueryTop.sql
+++ SQL/SQLQueryTop.sql
@@ -1,2 +1,8 @@
 --TOP--
-/*comando top ele vai filtrar e limitar a quantidade de um select /*
+/*comando top ele vai filtrar e limitar a quantidade de um select */
+--sintaxe--
+SELECT TOP 10*
+FROM tabela
+-------------------------------------------------------------------------
+SELECT TOP 10*
+FROM Production.Product
