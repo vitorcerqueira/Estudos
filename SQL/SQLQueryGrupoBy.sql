0f7d75f248f10ee965d0e824a9346e6855b9a491
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-19 12:03:28 -0300

    alterado

--- SQL/SQLQueryGrupoBy.sql
+++ SQL/SQLQueryGrupoBy.sql
@@ -28,4 +28,16 @@ SELECT productId, COUNT(ProductId) AS "CONTAGEM"
 FROM Sales.SalesOrderDetail
 GROUP BY ProductID
 
+-- vamos dizer que eu quero saber quantos nomes de cada nome temos cadastrado em nosso banco de dados
+SELECT firstname,COUNT(firstname)AS "CONTAGEM"
+FROM Person.Person
+GROUP BY FirstName
+
+--na tabela production.product eu quero saber a media de preço para os produtos que sao pratas(silver)
+SELECT color,AVG(listprice) AS "preço"
+FROM Production.Product
+WHERE color = 'silver'
+GROUP BY Color
+
+
 
