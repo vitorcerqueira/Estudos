9b167e236c5ec6549c5ee1bbc80c51aedfa6a384
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-19 12:01:12 -0300

    alterado

--- SQL/SQLQueryGrupoBy.sql
+++ SQL/SQLQueryGrupoBy.sql
@@ -8,10 +8,24 @@ SELECT coluna1,funcaoAgregacao(coluna2)
 FROM nometabela
 GRUPO BY coluna1;
 */
+
 SELECT*
 FROM Sales.SalesOrderDetail
 
 SELECT SpecialOfferID, SUM(unitPrice) AS "SOMA"
 FROM Sales.SalesOrderDetail
 GROUP BY SpecialOfferID
+
+SELECT SpecialOfferID, UnitPrice
+FROM Sales.SalesOrderDetail
+WHERE SpecialOfferID = 9
+
+--vamos dizer que eu quero saber quantos cada produto foi vendido ate hoje 
+SELECT* 
+FROM Sales.SalesOrderDetail
+
+SELECT productId, COUNT(ProductId) AS "CONTAGEM"
+FROM Sales.SalesOrderDetail
+GROUP BY ProductID
+
 
