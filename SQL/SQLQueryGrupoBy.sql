95e1d020b0d05e21710defd5ff286f2bcef10f0c
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 23:16:17 -0300

    alterado

---
+++ SQL/SQLQueryGrupoBy.sql
@@ -0,0 +1,10 @@
+/*GRUPO BY
+GRUPO BY é basicamente divide o resultado da sua pesquisa em grupos 
+para cada grupo voce pode aplicar uma funçao de agregaçao por exemplo:
+calcular a soma de intens
+contar o numero de itens naquele grupo 
+SINTAXE
+SELECT coluna1,funcaoAgregacao(coluna2)
+FROM nometabela
+GRUPO BY coluna1;
+*/
