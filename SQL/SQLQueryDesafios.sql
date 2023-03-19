393d39d7c5017d60ce5bd7e5f0bbec86b6a8d7fa
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-18 21:45:41 -0300

    alterado

--- SQL/SQLQueryDesafios.sql
+++ SQL/SQLQueryDesafios.sql
@@ -1,16 +1,25 @@
 --DESAFIO FUNDAMENTOS SQL --
-/*1- quantos produtos temos cadastrado no sistema que custam mais que 1500 dolares?*/
+/*1- Quantos produtos temos cadastrado no sistema que custam mais que 1500 dolares?*/
 
 SELECT COUNT (ListPrice)
 FROM Production.Product
 WHERE ListPrice > '1500' 
 
-/*2- quantas pessoas temos com o sobrenome que inicia com a letra p*/
+/*2- Quantas pessoas temos com o sobrenome que inicia com a letra p*/
 SELECT COUNT (LastName)
 FROM Person.Person
 WHERE LastName like 'P%'
 
-/*2- */
+/*3- Em quantas cidades unicas estao cadastrados nossos clientes*/
+SELECT COUNT (DISTINCT city)
+FROM Person.Address
+
+/*4- Quais sao as cidades unicas que temos cadastrados em nosso sistema*/
+SELECT DISTINCT City
+FROM Person.Address
+
+
+ 
 
 
 
