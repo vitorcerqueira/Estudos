84af6fc4b14503857cb3d4a5129458b9c01fc101
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-22 20:08:35 -0300

    alterado

--- SQL/SQLQueryHaving.sql
+++ SQL/SQLQueryHaving.sql
@@ -12,10 +12,18 @@ HAVING condicao;
 é que o GROUP BY é aplicado depois que os dados ja foram agrupados, enquanto o WHERE é aplicado antes 
 dos dados serem agrupados*/ 
 
-
 --vamos dizer que queremos saber quais nomes no sistema tem uma ocorrencia maior que 10 vezes 
 SELECT firstName,COUNT(firstname)AS "quantidade"
 FROM Person.Person
 GROUP BY firstName
 HAVING COUNT(firstName) >10
+
+--quero saber quais produtos que no total de vendas estao entre 162k a 500k
+SELECT TOP 10*
+FROM sales.SalesOrderDetail
+
+SELECT productid,SUM(linetotal) AS "Total"
+FROM sales.SalesOrderDetail
+GROUP BY ProductID
+HAVING SUM(linetotal) between 162000 and 500000
 
