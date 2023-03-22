00a72bd2c9e917a2a93ea46eb2fb25e35c7ba63a
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-03-22 20:01:51 -0300

    alterado

--- SQL/SQLQueryHaving.sql
+++ SQL/SQLQueryHaving.sql
@@ -11,3 +11,11 @@ HAVING condicao;
 /*DIFERENÇA entre HAVING e WHERE 
 é que o GROUP BY é aplicado depois que os dados ja foram agrupados, enquanto o WHERE é aplicado antes 
 dos dados serem agrupados*/ 
+
+
+--vamos dizer que queremos saber quais nomes no sistema tem uma ocorrencia maior que 10 vezes 
+SELECT firstName,COUNT(firstname)AS "quantidade"
+FROM Person.Person
+GROUP BY firstName
+HAVING COUNT(firstName) >10
+
