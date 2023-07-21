eb67682c28e14012c3d6d84a2458cef3e7f59605
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-21 17:12:27 -0300

    alterado

--- JavaScript/Recursos nativos do navegador/index.js
+++ JavaScript/Recursos nativos do navegador/index.js
@@ -1,5 +1,14 @@
 let nome = "Isaac"
+
 console.log(nome)
 console.error(nome)
 console.warn(nome)
+
+// widow.alert(nome)
+// nome =widow.prompt("insira o seu nome:")
+let confirmacao = window.confirm("confirma essa açao?")
+console.log(confirmacao)
+
+
+
 
