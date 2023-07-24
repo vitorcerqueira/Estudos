39ad24684639fd2ffa43607a5499b17c554f2302
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-24 11:37:39 -0300

    alterado

---
+++ JavaScript/Calculadora de 4 Operações/calculadora.js
@@ -0,0 +1,19 @@
+const x = prompt("informe o primeiro numero");
+const y = prompt("informe o segundo numero");
+
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+
+alert(
+  "resultado:\n" +
+    "\nsome:" +
+    soma +
+    "\nsubtracao:" +
+    subtracao +
+    "\nmultiplicacao:" +
+    multiplicacao +
+    "\ndivisao:" +
+    divisao
+);
