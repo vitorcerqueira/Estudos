ae6b84668dd6f4bdca5b26c4092e28527c226763
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-24 12:18:58 -0300

    concluido

---
+++ Logica de programação/Comparação de dados/Comparação de dados.js
@@ -0,0 +1,41 @@
+// retornando um resultado em boleano (true ou false)
+// == e === os dois sinais de igual é para mostrar o resultado igual independente se é strig ou nao
+// os tres sinais de igual so vai mostrar o resultado se for numero
+// != e !== primeiro sinal é diferente, e ja o segundo é diferente ou igual
+// >,>=, <,<= sinais maior, maior ou igual, menor, ou menor ou igual
+
+//exemplos
+// 3 == 3
+// true
+// 4 == 3
+// false
+// 3*3 + 1 == 10
+//true
+//10 === 10
+//true
+//10 === 11
+//false
+// 10 == "10"
+// true
+// 10 === "10"
+//false
+
+// 3 != 3
+//false
+// 5 != 3
+// true
+// 5 !="5"
+//false
+//5 !=="5"
+//true
+
+//4 > 3
+// true
+//5 > 10
+//false
+//5 >= 5
+//true
+//6 <= 7
+//true
+//6 < 6
+//false
