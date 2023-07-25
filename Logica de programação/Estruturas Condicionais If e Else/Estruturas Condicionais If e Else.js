f795f3865dbf8617bbc236ae170153a56cdf83ae
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-25 19:54:31 -0300

    criado

---
+++ Logica de programação/Estruturas Condicionais If e Else/Estruturas Condicionais If e Else.js
@@ -0,0 +1,11 @@
+const idade = prompt("informe sua idade:");
+
+if (idade >= 18) {
+  alert("voce é maior de idade");
+} else {
+  alert("voce é menor de idade");
+
+  if (idade <= 17) {
+    alert("voce é criança");
+  }
+}
