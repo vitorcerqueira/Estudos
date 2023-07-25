5a6c2fb3114f81076845fce4fa92cae23afe539c
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-25 13:13:40 -0300

    criado

---
+++ Logica de programação/Operadores lógicos/Operadores lógicos.js
@@ -0,0 +1,18 @@
+//OPERADORES LOGICOS
+// E (AND)
+// Ou (OR)
+// Não (NOT)
+
+//TABELA VERDADE - E (AND)
+// so é verdadeiro se ambas as partes forem verdadeiras
+//true && true === true
+//true && false === false
+//falso && true === false
+//falso && false === false
+
+//TABELA VERDADE - Ou (OR)
+//
+//true && true === true
+//true && false === false
+//falso && true === false
+//falso && false === false
