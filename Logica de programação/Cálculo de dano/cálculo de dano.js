f268e392051646ba898cce808091d3db5408ab3f
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-26 16:42:49 -0300

    alterado

--- Logica de programação/Cálculo de dano/cálculo de dano.js
+++ Logica de programação/Cálculo de dano/cálculo de dano.js
@@ -5,3 +5,11 @@ const defensor = prompt("qual é o nome do personagem defensor?");
 let pontosDeVida = parseFloat(prompt("quantos pontos de vida ele possui?"));
 const poderDeDefesa = parseFloat(prompt("qual é o seu poder de defesa?"));
 const possuiEscudo = prompt("ele possui um escudo ? (sim/nao)");
+
+let danoCausado = 0;
+
+if (poderDeAtaque > poderDeDefesa && possuiEscudo === "nao") {
+  danoCausado = poderDeAtaque - poderDeDefesa;
+} else if (poderDeAtaque > poderDeDefesa && possuiEscudo === "sim") {
+  danoCausado = (poderDeAtaque - poderDeDefesa) / 2;
+}
