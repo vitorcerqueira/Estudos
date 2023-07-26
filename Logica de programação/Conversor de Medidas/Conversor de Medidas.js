99d854d6e27c31d0b6e4a362ee1d659134df5f83
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-26 18:52:33 -0300

    finalizado

--- Logica de programação/Conversor de Medidas/Conversor de Medidas.js
+++ Logica de programação/Conversor de Medidas/Conversor de Medidas.js
@@ -13,14 +13,22 @@ const unidade = prompt(
 switch (unidade) {
   case "1":
     alert("resultado: " + medida + "m =" + medida * 1000 + "mm");
+    break
   case "2":
     alert("resultado: " + medida + "m =" + medida * 100 + "cm");
+    break
   case "3":
     alert("resultado: " + medida + "m =" + medida * 10 + "dm");
+    break
   case "4":
     alert("resultado: " + medida + "m =" + medida / 10 + "dam");
+    break
   case "5":
     alert("resultado: " + medida + "m =" + medida / 100 + "hm");
+    break
   case "6":
     alert("resultado: " + medida + "m =" + medida / 1000 + "km");
+    break
+    default:
+    alert("opção invalida.")  
 }
