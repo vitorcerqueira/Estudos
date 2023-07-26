fe5dd4a1d2f4753500e5e710c3de92e4f1c5d10d
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-26 18:48:07 -0300

    alterado

--- Logica de programação/Conversor de Medidas/Conversor de Medidas.js
+++ Logica de programação/Conversor de Medidas/Conversor de Medidas.js
@@ -9,3 +9,18 @@ const unidade = prompt(
     "\n5. hectômetros (hm)" +
     "\n6. quilômetros (km)"
 );
+
+switch (unidade) {
+  case "1":
+    alert("resultado: " + medida + "m =" + medida * 1000 + "mm");
+  case "2":
+    alert("resultado: " + medida + "m =" + medida * 100 + "cm");
+  case "3":
+    alert("resultado: " + medida + "m =" + medida * 10 + "dm");
+  case "4":
+    alert("resultado: " + medida + "m =" + medida / 10 + "dam");
+  case "5":
+    alert("resultado: " + medida + "m =" + medida / 100 + "hm");
+  case "6":
+    alert("resultado: " + medida + "m =" + medida / 1000 + "km");
+}
