adf888ee4372a1e461710ee87f1fc23d801c57d2
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-26 18:40:29 -0300

    criado

---
+++ Logica de programação/Conversor de Medidas/Conversor de Medidas.js
@@ -0,0 +1,11 @@
+const medida = prompt("insira uma medida em metros: ");
+
+const unidade = prompt(
+  "Para qual unidade de medida deseja converter?" +
+    "\n1. milimetros (mm)" +
+    "\n2. centímetros (cm)" +
+    "\n3. decímetros (dm)" +
+    "\n4. decâmetros (dam)" +
+    "\n5. hectômetros (hm)" +
+    "\n6. quilômetros (km)"
+);
