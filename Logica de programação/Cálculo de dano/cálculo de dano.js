c8d65e232a0cc14ad2dafa4ccc6732a26b439972
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-26 16:38:31 -0300

    alterado

---
+++ Logica de programação/Cálculo de dano/cálculo de dano.js
@@ -0,0 +1,7 @@
+const atacante = prompt("qual é o nome do personagem atacante?");
+const poderDeAtaque = parseFloat(prompt("qual é o seu poder de ataque?"));
+
+const defensor = prompt("qual é o nome do personagem defensor?");
+let pontosDeVida = parseFloat(prompt("quantos pontos de vida ele possui?"));
+const poderDeDefesa = parseFloat(prompt("qual é o seu poder de defesa?"));
+const possuiEscudo = prompt("ele possui um escudo ? (sim/nao)");
