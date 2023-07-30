97e422d434f13a034def1063b94820a6c43b692e
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-07-29 22:55:09 -0300

    alterado

--- Logica de programação/Visitando novas cidades/Visitando novas cidades.js
+++ Logica de programação/Visitando novas cidades/Visitando novas cidades.js
@@ -2,4 +2,18 @@ const turista = prompt("E ai, turista! Qual é o seu nome?")
 let cidades = ""
 let contagem = 0
 
-let continuar =
+let continuar = prompt ("voce vistou alguma cidade? (sim/nao)")
+
+while (continuar === "sim") {
+  let cidade = prompt ("qual é o nome da cidade visitada?")
+  //cidades = cidades +...
+  cidades += "-" + cidade + "\n"
+  contagem++
+  continuar = prompt("voce visitou alguma outra cidade (sim/nao)")
+
+}
+alert{
+  "turista: " + turista + 
+  "\nQuantidade de cidades visitadas: " + contagem +
+  "\nCidades visitadas:\n" + cidades
+}
