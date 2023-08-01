cf160bd969ac2ee0ff6f7695df9c0681b1a39d04
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-01 11:05:19 -0300

    alterado

---
+++ Logica de programação/Menu interativo/Menu interativo.js
@@ -0,0 +1,14 @@
+let opcao = "5"
+
+do {
+  opcao = prompt(
+    "seja bem vindo(a)\n" +
+    "\nEscolha uma uma das opçoes abaixo:" +
+    "\n1. opção Um" +
+    "\n2. opção Dois" +
+    "\n3. opção Três" +
+    "\n4. opção Quatro" +
+    "\n5. opção Encerrar" 
+  )
+
+} while (opcao !==="5")
