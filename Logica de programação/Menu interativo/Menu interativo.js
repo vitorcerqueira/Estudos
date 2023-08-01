10357abca64a9a84c2a7c7c6942966b9417a8d75
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-01 11:14:10 -0300

    alterado

--- Logica de programação/Menu interativo/Menu interativo.js
+++ Logica de programação/Menu interativo/Menu interativo.js
@@ -1,14 +1,31 @@
-let opcao = "5"
+let opcao = "5";
 
 do {
   opcao = prompt(
     "Seja bem-vindo(a)\n" +
-    "\nEscolha uma uma das opçoes abaixo:" +
-    "\n1. Opção Um" +
-    "\n2. Opção Dois" +
-    "\n3. Opção Três" +
-    "\n4. Opção Quatro" +
-    "\n5. Encerrar" 
-  )
+      "\nEscolha uma uma das opçoes abaixo:" +
+      "\n1. Opção Um" +
+      "\n2. Opção Dois" +
+      "\n3. Opção Três" +
+      "\n4. Opção Quatro" +
+      "\n5. Encerrar"
+  );
 
-} while (opcao !== "5")
+  switch (opcao) {
+    case "1":
+      alert("Você escolheu a opção 1");
+      break;
+    case "2":
+      alert("Você escolheu a opção 2");
+      break;
+    case "3":
+      alert("Você escolheu a opção 3");
+      break;
+    case "4":
+      alert("Você escolheu a opção 4");
+      break;
+    case "5":
+      alert("Você escolheu a opção 5");
+      break;
+  }
+} while (opcao !== "5");
