59ffc1ecb5f36a2f962b842e2ad833b4bfdcce5b
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-03 14:51:22 -0300

    alterado

--- Logica de programação/Robô da tabuada/Robô da tabuada.js
+++ Logica de programação/Robô da tabuada/Robô da tabuada.js
@@ -7,3 +7,5 @@ let resultado = "";
 for (let fator = 1; fator <= 20; fator++) {
   resultado += " ->" + numero + " * " + fator + " = " + numero * fator + "\n";
 }
+
+alert("Resultado da tabuada de " + numero + ":\n\n" + resultado);
