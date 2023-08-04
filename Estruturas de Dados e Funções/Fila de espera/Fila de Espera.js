c6653cd0a9c9d2a15b35bd449ced6d678abbd424
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-04 16:55:20 -0300

    alterado

--- Estruturas de Dados e Funções/Fila de espera/Fila de Espera.js
+++ Estruturas de Dados e Funções/Fila de espera/Fila de Espera.js
@@ -1,31 +1,38 @@
-let fila = [];
-let opcao = " ";
+let fila = []
+let opcao = " "
 
-do {
-  let pacientes = " ";
+
+
+do{
+  let pacientes = " "
   for (let i = 0; i < fila.length; i++) {
-    pacientes += i + 1 + "º - " + fila[i] + "\n";
-  }
+    pacientes += (i + 1) + "º - " + fila[i] + "\n"
+}
 
-  opcao = prompt(
-    "Pacientes:\n" +
-      pacientes +
-      "\nEscolha uma ação:\n1. Novo pacinte\n2. consultar paciente\n3. Sair"
-  );
+opcao = prompt(
+  "Pacientes:\n" + pacientes + 
+"\nEscolha uma ação:\n1. Novo pacinte\n2. consultar paciente\n3. Sair" 
+)
 
-  switch (opcao) {
-    case "1":
-      const novoPaciente = prompt("qual é o nome do paciente?");
-      fila.push(novoPaciente);
-      break;
-    case "2":
-      const pacienteConsultado = fila.shift();
-      alert(pacienteConsultado + "foi removido da fila");
-      break;
-    case "3":
-      alert("encerrando...");
-      break;
-    default:
-      alert("opção invalida!");
-  }
+switch (opcao) {
+  case "1":
+    const novoPaciente = prompt("Qual é o nome do paciente?")
+    fila.push(novoPaciente)
+    break
+  case "2":
+    const pacienteConsultado = fila.shift()
+    if (!pacienteConsultado) {
+      alert("Não há pacientes na fila!")
+    } else {
+      alert(pacienteConsultado + " foi removido da fila.")
+    }
+    break
+  case "3":
+    alert("Encerrando...")
+    break
+  default:
+    alert("Opção inválida!")
+    break
+}
 } while (opcao !== "3");
+
