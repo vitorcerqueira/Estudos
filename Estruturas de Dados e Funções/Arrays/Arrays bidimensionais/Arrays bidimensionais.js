6328ea7b679b36e58d97747653868a1c77d420c3
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-07 12:34:08 -0300

    alterado

--- Estruturas de Dados e Funções/Arrays/Arrays bidimensionais/Arrays bidimensionais.js
+++ Estruturas de Dados e Funções/Arrays/Arrays bidimensionais/Arrays bidimensionais.js
@@ -16,3 +16,10 @@ console.log(arr[1][1])    // Saída: 42
 console.log(arr[2])       // Saída: [ [ '3º nível, 1º item', 'Olá, mundo!' ], [ '3º nível, 2º item', 'Oi, mundo!' ] ]
 console.log(arr[2][0])    // Saída: [ '3º nível, 1º item', 'Olá, mundo!' ]
 console.log(arr[2][1][1]) // Saída: 'Oi, mundo!'
+// Podemos ver claramente a estrutura de uma matriz em uma tabela
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
+console.table(matriz)
