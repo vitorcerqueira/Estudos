6043330c1b5672a6fbd1d62347672cee3d830aad
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-18 16:19:46 -0300

    alterado

--- Estruturas de Dados e Funções/Escopo/Escopo.js
+++ Estruturas de Dados e Funções/Escopo/Escopo.js
@@ -4,56 +4,56 @@
 
 // Variáveis no escopo mais externo podem ser
 // usadas dentro de escopos mais internos
-let pokemon = "Charmander"
+let pokemon = "Charmander";
 
 function evoluir() {
-  pokemon = "Charmeleon"
+  pokemon = "Charmeleon";
 }
 
-console.log(pokemon)
-evoluir()
-console.log(pokemon)
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
 
 // Variáveis no escopo mais interno não podem ser
 // usadas fora dele em escopos mais internos
 function criarAnimal() {
-  let animal = "Gato"
+  let animal = "Gato";
 }
 
-criarAnimal()
-console.log(animal) // Gera erro
+criarAnimal();
+console.log(animal); // Gera erro
 
-#//Escopo com var e let (ou const) 
+//#Escopo com var e let (ou const)
 // Apenas variáveis declaradas com var ficam
 // disponíveis em um escopo mais externo
 function avaliarNota(nota) {
   if (nota > 60) {
-    var aprovado = true
-    let situacao = "Aprovado"
+    var aprovado = true;
+    let situacao = "Aprovado";
   } else {
-    var aprovado = false
-    let situacao = "Reprovado"
+    var aprovado = false;
+    let situacao = "Reprovado";
   }
 
-  console.log(nota)
-  console.log(aprovado)
-  console.log(situacao) // Gera erro
+  console.log(nota);
+  console.log(aprovado);
+  console.log(situacao); // Gera erro
 }
-avaliarNota(83)
-avaliarNota(49)
+avaliarNota(83);
+avaliarNota(49);
 
 // Mas mesmo o var não consegue "sair" para fora do escopo da função
 function ola() {
-  var texto = "Olá, mundo!"
+  var texto = "Olá, mundo!";
 }
-console.log(texto) // Gera erro
+console.log(texto); // Gera erro
 
 //#Inicialização de variáveis com var e let (ou const)
 // Apenas variáveis declaradas com var são carregadas
 // sempre no começo do código, acima de todo o resto
-console.log(nome)
-console.log(sobrenome)
-var nome = "Isaac"
-let sobrenome = "Pontes"
-console.log(nome)
-console.log(sobrenome)
+console.log(nome);
+console.log(sobrenome);
+var nome = "Isaac";
+let sobrenome = "Pontes";
+console.log(nome);
+console.log(sobrenome);
