a31a3d97652d4af74f3f2a7ebea0dd9b60a13655
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-18 15:33:57 -0300

    alterado

--- Estruturas de Dados e Funções/Parâmetros de uma função/Parâmetros de uma função.js
+++ Estruturas de Dados e Funções/Parâmetros de uma função/Parâmetros de uma função.js
@@ -13,4 +13,16 @@ function dizerola(nome){
 function soma(a, b, c, d, e){
   alert("O resutado da soma é " + (a + b + c + d + e ))
 }
-soma(7, 6, 4, 1, 9)
+//soma(7, 6, 4, 1, 9)
+
+function criarUsuario(nome, email, senha, tipo = "admin"){
+  const usuario = {
+    nome, //quando for o mesmo que nome:nome
+    email,
+    senha,
+    tipo,
+  }
+  console.log(usuario)
+}
+
+criarUsuario("isaac", "isaac@gmail.com", "1234")
