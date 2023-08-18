e2b7c0d88c75d06821aa01a4f9006b4380e6fa59
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-18 15:25:45 -0300

    alterado

--- Estruturas de Dados e Funções/Parâmetros de uma função/Parâmetros de uma função.js
+++ Estruturas de Dados e Funções/Parâmetros de uma função/Parâmetros de uma função.js
@@ -1,1 +1,16 @@
-function dobro() {}
+function dobro(x) {
+  alert("O dobro de " + x + " é " + x * 2);
+}
+
+//dobro(15);
+//dobro(25);
+
+function dizerola(nome){
+  alert("ola," + nome + "!")
+}
+//dizerola("Vitor")
+
+function soma(a, b, c, d, e){
+  alert("O resutado da soma é " + (a + b + c + d + e ))
+}
+soma(7, 6, 4, 1, 9)
