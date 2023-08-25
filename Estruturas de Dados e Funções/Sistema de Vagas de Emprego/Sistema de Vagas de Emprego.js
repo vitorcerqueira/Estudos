5f9479bb1f8055faf46b93c4db9a1572ed6fbf33
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-25 12:21:22 -0300

    alterado

---
+++ Estruturas de Dados e Funções/Sistema de Vagas de Emprego/Sistema de Vagas de Emprego.js
@@ -0,0 +1,12 @@
+const vagas = [];
+
+function listarVagas() {
+  const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => {
+    textoFinal += indice + ". ";
+    textoFinal += vaga.nome;
+    textoFinal += " (" + vaga.candidatos.length + " candidatos)\n";
+    return textoFinal;
+  }, "");
+
+  alert(vagasEmTexto);
+}
