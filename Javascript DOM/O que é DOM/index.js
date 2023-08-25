65008807efdf72a4f4cb6744dac2853543324a65
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-08-25 15:36:28 -0300

    alteraçao

---
+++ Javascript DOM/O que é DOM/index.js
@@ -0,0 +1,8 @@
+// Encontrar o elemento <h1> e mudar o seu texto
+var titulo = document.querySelector('h1');
+titulo.textContent = 'Olá a todos!';
+
+// Adicionar um novo parágrafo
+var novoParagrafo = document.createElement('p');
+novoParagrafo.textContent = 'Isso é uma adição ao DOM.';
+document.body.appendChild(novoParagrafo);
