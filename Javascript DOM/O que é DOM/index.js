4f9fa8f8df4d4e0f3541be11b005d97ec603f38a
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-28 15:02:34 -0300

    alterado

--- Javascript DOM/O que é DOM/index.js
+++ Javascript DOM/O que é DOM/index.js
@@ -2,7 +2,7 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = 'Olá a todos!';
 
-// Adicionar um novo parágrafo
+// Adicionar um novo parágrafo.
 var novoParagrafo = document.createElement('p');
 novoParagrafo.textContent = 'Isso é uma adição ao DOM.';
 document.body.appendChild(novoParagrafo);
