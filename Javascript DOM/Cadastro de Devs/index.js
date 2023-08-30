45eb520d5e4a13713c7626d6ac74da3de4b51b9d
vitor cerqueira <vitor94.cerqueira@gmail.com>
2023-08-30 11:47:05 -0300

    alterado

---
+++ Javascript DOM/Cadastro de Devs/index.js
@@ -0,0 +1,16 @@
+function createLabel(text, htmlFor) {
+  const label = document.createElement('label')
+  label.htmlFor = htmlFor
+  label.innerText = text
+  return label
+}
+
+function createInput(id, value, name, type = 'text', placeholder = '') {
+  const input = document.createElement('input')
+  input.id = id
+  input.value = value
+  input.name = name
+  input.type = type
+  input.placeholder = placeholder
+  return input
+}
