88ee4182cc940fffaf5832242fdd819ae9fcce1d
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-09-01 10:38:31 -0300

    alterado

--- Javascript DOM/Manipulando Atributos com Javascript/index.js
+++ Javascript DOM/Manipulando Atributos com Javascript/index.js
@@ -6,3 +6,19 @@ document.getElementById('value').addEventListener('click', function () {
   console.log(input.value)
   console.log(input.getAttribute('value'))
 })
+document.getElementById('type').addEventListener('click', function () {
+  // input.type = input.type !== 'date' ? 'date' : 'text'
+  input.setAttribute('type', 'radio')
+})
+document.getElementById('placeholder').addEventListener('click', function () {
+  input.placeholder = 'Digita algo...'
+})
+document.getElementById('disable').addEventListener('click', function () {
+  input.setAttribute('disabled', !input.disabled)
+})
+document.getElementById('data').addEventListener('click', function () {
+  const data = input.dataset.somethingElse
+  console.log("O valor do atributo data-something-else é: " + data)
+  input.dataset.somethingElse = 'Algum outro valor'
+  console.log("O valor do atributo data-something-else agora é: " + input.dataset.somethingElse)
+})
