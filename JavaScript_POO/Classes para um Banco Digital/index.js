caa9ae3790724b6368e17cbbafc747c1895b3b4e
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-09-22 19:53:10 -0300

    alterado

---
+++ JavaScript_POO/Classes para um Banco Digital/index.js
@@ -0,0 +1,20 @@
+const App = require("./App")
+
+App.createUser("isaac@email.com", "Isaac Pontes")
+App.createUser("lucas@email.com", "Lucas Queiroga")
+App.createUser("juliana@email.com", "Juliana Conde")
+
+App.deposit("isaac@email.com", 100)
+
+App.transfer("isaac@email.com", "lucas@email.com", 20)
+
+App.changeLoanFee(10)
+App.takeLoan("juliana@email.com", 2000, 24)
+
+console.log(App.findUser("isaac@email.com"))
+console.log(App.findUser("isaac@email.com").account)
+console.log(App.findUser("lucas@email.com"))
+console.log(App.findUser("lucas@email.com").account)
+console.log(App.findUser("juliana@email.com"))
+console.log(App.findUser("juliana@email.com").account)
+console.log(App.findUser("juliana@email.com").account.loans[0].installments)
