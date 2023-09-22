8304d539cbadbc0cca670203d9e4909fe4166ee4
vitorcerqueira <vitor94.cerqueira@gmail.com>
2023-09-22 20:03:35 -0300

    salvar

--- JavaScript_POO/Classes para um Banco Digital/index.js
+++ JavaScript_POO/Classes para um Banco Digital/index.js
@@ -1,20 +1,20 @@
-const App = require("./App")
+const App = require("./App");
 
-App.createUser("isaac@email.com", "Isaac Pontes")
-App.createUser("lucas@email.com", "Lucas Queiroga")
-App.createUser("juliana@email.com", "Juliana Conde")
+App.createUser("Gabriela@email.com", "Gabriela Cerqueira");
+App.createUser("Vitor@email.com", "Vitor Cerqueira");
+App.createUser("Enzo@email.com", "Enzo Cerqueira");
 
-App.deposit("isaac@email.com", 100)
+App.deposit("Gabriela@email.com", 100);
 
-App.transfer("isaac@email.com", "lucas@email.com", 20)
+App.transfer("Gabriela@email.com", "Vitor@email.com", 20);
 
-App.changeLoanFee(10)
-App.takeLoan("juliana@email.com", 2000, 24)
+App.changeLoanFee(10);
+App.takeLoan("Enzo@email.com", 2000, 24);
 
-console.log(App.findUser("isaac@email.com"))
-console.log(App.findUser("isaac@email.com").account)
-console.log(App.findUser("lucas@email.com"))
-console.log(App.findUser("lucas@email.com").account)
-console.log(App.findUser("juliana@email.com"))
-console.log(App.findUser("juliana@email.com").account)
-console.log(App.findUser("juliana@email.com").account.loans[0].installments)
+console.log(App.findUser("Gabriela@email.com"));
+console.log(App.findUser("Gabriela@email.com").account);
+console.log(App.findUser("Vitor@email.com"));
+console.log(App.findUser("Vitor@email.com").account);
+console.log(App.findUser("Enzo@email.com"));
+console.log(App.findUser("Enzo@email.com").account);
+console.log(App.findUser("Enzo@email.com").account.loans[0].installments);
